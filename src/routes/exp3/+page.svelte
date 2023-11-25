9cd0e812c9d814235729e8cc4d64d9c74bc522a6
lulagalvez <luisgalvezuk@gmail.com>
2023-11-25 16:02:01 -0300

    Agregados experimentos 2 y 3

---
+++ src/routes/exp3/+page.svelte
@@ -0,0 +1,255 @@
+<script lang="ts">
+    import "../global.css";
+    import { onMount, onDestroy } from "svelte";
+    import { tweened } from "svelte/motion";
+    import { cubicOut } from "svelte/easing";
+    import { browser } from "$app/environment";
+    import { goto } from "$app/navigation";
+
+    const rotation = tweened(0, {
+        duration: 3000,
+    });
+
+    let userAlias = ""; // Variable to store the alias
+    let displayImage = true;
+    let currentCycle = 1; // Inicia en el primer ciclo
+    let showReturnButton = false; // Controla la visibilidad del botón de retorno
+    interface CycleData {
+        alias: string;
+        experiment: string;
+        movementType: string; // Nueva propiedad
+        ArrowDown: number;
+        ArrowLeft: number;
+        ArrowRight: number;
+        total: number;
+        dateTime: string;
+    }
+    let cycleTimes: CycleData[] = [];
+    let keyDownTimes: { [key: string]: number } = {
+        ArrowDown: 0,
+        ArrowLeft: 0,
+        ArrowRight: 0,
+    };
+    let keyPressStart: { [key: string]: number | null } = {};
+
+    const movements = [0, 1, -1];
+
+    // Función para mezclar el arreglo
+    function shuffle(array: number[]): number[] {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    }
+
+    // Mezclar los movimientos
+    const shuffledMovements = shuffle([...movements]);
+
+    let currentMovement = 0; // Variable para almacenar el tipo de movimiento actual
+    onMount(() => {
+        userAlias = localStorage.getItem("alias") || "Unknown";
+
+        const startCycle = () => {
+            // Actualiza el tipo de movimiento al inicio de cada ciclo
+            currentMovement = shuffledMovements[currentCycle - 1];
+            // Restablece las variables de tiempo
+            keyDownTimes = { ArrowDown: 0, ArrowLeft: 0, ArrowRight: 0 };
+            keyPressStart = {};
+            // Intervalo para rotación
+            const interval = setInterval(() => {
+                rotation.update((n) => n + 360 * currentMovement);
+            }, 3000);
+
+            setTimeout(() => {
+                clearInterval(interval);
+                displayImage = false;
+
+                // Inicia escucha de eventos de teclado solo si es necesario
+                if (browser && currentCycle <= 3) {
+                    window.addEventListener("keydown", handleKeyDown);
+                    window.addEventListener("keyup", handleKeyUp);
+                }
+
+                // Espera 30 segundos más para cambiar de imagen o terminar el ciclo
+                setTimeout(() => {
+                    if (currentCycle < 3) {
+                        saveAndExportCycleData();
+                        currentCycle++;
+                        displayImage = true;
+                        startCycle();
+                    } else {
+                        saveAndExportCycleData();
+                        showReturnButton = true;
+                    }
+                }, 30000);
+            }, 30000);
+
+            return () => {
+                clearInterval(interval);
+                if (browser) {
+                    window.removeEventListener("keydown", handleKeyDown);
+                    window.removeEventListener("keyup", handleKeyUp);
+                }
+            };
+        };
+
+        startCycle();
+    });
+
+    async function postToSheetAPI() {
+        try {
+            const response = await fetch(
+                "https://sheet.best/api/sheets/81947b65-2fbd-4577-8ab8-583c4ce45b2f",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(cycleTimes),
+                },
+            );
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const result = await response.json();
+            console.log("Data posted successfully:", result);
+        } catch (error) {
+            console.error("Error posting data:", error);
+        }
+    }
+
+    function saveAndExportCycleData() {
+        const total = parseFloat(
+            Object.values(keyDownTimes)
+                .reduce((a, b) => a + b, 0)
+                .toFixed(3),
+        );
+        const dateTime = new Date().toISOString();
+
+        let movementType = "";
+        switch (currentMovement) {
+            case 1:
+                movementType = "Horario";
+                break;
+            case -1:
+                movementType = "Antihorario";
+                break;
+            default:
+                movementType = "Sin Girar";
+        }
+
+        cycleTimes.push({
+            alias: userAlias,
+            movementType,
+            experiment: "exp1", // Agregado aquí
+            ArrowLeft: keyDownTimes["ArrowLeft"],
+            ArrowDown: keyDownTimes["ArrowDown"],
+            ArrowRight: keyDownTimes["ArrowRight"],
+            total,
+            dateTime,
+        });
+
+        if (currentCycle === 3) {
+            postToSheetAPI()
+        }
+    }
+
+    function handleKeyDown(event: KeyboardEvent) {
+        if (
+            !displayImage &&
+            ["ArrowDown", "ArrowLeft", "ArrowRight"].includes(event.key)
+        ) {
+            keyPressStart[event.key] =
+                keyPressStart[event.key] || performance.now();
+        }
+    }
+
+    function handleKeyUp(event: KeyboardEvent) {
+        if (!displayImage && keyPressStart[event.key]) {
+            // Redondea a 3 decimales y convierte a número
+            keyDownTimes[event.key] += parseFloat(
+                (
+                    (performance.now() - keyPressStart[event.key]!) /
+                    1000
+                ).toFixed(3),
+            );
+            keyPressStart[event.key] = null;
+        }
+    }
+
+    function exportToCSV() {
+        const headers =
+            "Alias,Tipo de Movimiento,Izquierda,Abajo,Derecha,Total,Fecha_hora\n";
+        const csvContent = `data:text/csv;charset=utf-8,${headers}${cycleTimes
+            .map(
+                ({
+                    alias,
+                    movementType,
+                    ArrowLeft,
+                    ArrowDown,
+                    ArrowRight,
+                    total,
+                    dateTime,
+                }) =>
+                    `${alias},${movementType},${ArrowLeft},${ArrowDown},${ArrowRight},${total},${dateTime}`,
+            )
+            .join("\n")}`;
+        const encodedUri = encodeURI(csvContent);
+        const link = document.createElement("a");
+        link.setAttribute("href", encodedUri);
+        link.setAttribute("download", "key_press_times.csv");
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
+
+    function goToMainMenu() {
+        goto("/"); // Reemplaza '/' con la ruta del menú principal
+    }
+</script>
+
+<!-- Resto del HTML y estilos -->
+
+<div class="center">
+    {#if displayImage}
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <img
+            src="/images/exp3_adapt.png"
+            class="image"
+            style="transform: rotate({$rotation}deg);"
+        />
+    {:else}
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <img src="/images/exp3_test.png" class="image-static" />
+    {/if}
+
+    {#if showReturnButton}
+        <button on:click={goToMainMenu}>Volver al menú principal</button>
+    {/if}
+</div>
+
+<style>
+    .center {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 90vh; /* Set full viewport height */
+    }
+
+    .image {
+        width: 636px; /* Adjust size as needed */
+        height: 636px; /* Adjust size as needed */
+        object-fit: cover; /* Maintain aspect ratio of the image */
+        transform-origin: center;
+        transform: rotate(90deg);
+    }
+
+    .image-static {
+        width: 636px; /* Adjust size as needed */
+        height: 636px; /* Adjust size as needed */
+        object-fit: cover; /* Maintain aspect ratio of the image */
+    }
+</style>

802c103e831116f1e5b3592729d1eaf97bc3040e
lulagalvez <luisgalvezuk@gmail.com>
2023-11-27 11:57:17 -0300

    CSS para navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,36 @@
 	<a href="/settings">Configuración</a>
 </nav>
 
-<slot></slot>
+<slot />
+
+<style>
+	/* Style for the navigation */
+	nav {
+		background-color: #333;
+		padding: 10px;
+		display: flex;
+		justify-content: space-around;
+	}
+
+	/* Style for the navigation links */
+	nav a {
+		color: #fff;
+		text-decoration: none;
+		padding: 8px 16px;
+		border-radius: 4px;
+		transition: background-color 0.3s ease;
+	}
+
+	/* Hover effect for navigation links */
+	nav a:hover {
+		background-color: #555;
+	}
+
+	/* Styling for the content within the layout */
+	slot {
+		margin-top: 20px;
+		padding: 20px;
+		border: 1px solid #ccc;
+		border-radius: 4px;
+	}
+</style>
